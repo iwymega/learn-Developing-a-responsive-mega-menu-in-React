f2e0c0bd65967877bbe429cc07c812f0c105bee5
I Wayan Megantara <iwymega@gmail.com>
2024-03-17 09:09:19 +0800

    until Rendering dropdown content links

--- src/components/MegaMenu.jsx
+++ src/components/MegaMenu.jsx
@@ -1,7 +1,18 @@
+import { menuData } from '../menuData';
+import MenuItem from './MenuItem';
+
 const MegaMenu = () => {
     return (
         <div className="nav__container">
-            <nav>mega menu items</nav>
+            <nav>
+                <ul>
+                    {menuData.map(({ label, href, children }, index) => {
+                        return (
+                            <MenuItem key={index} {...{ label, href, children }} />
+                        );
+                    })}
+                </ul>
+            </nav>
         </div>
     );
 };
---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+import Container from './Container';
+import DropdownContent from './DropdownContent';
+
+const MenuItem = ({ label, href, children }) => {
+    return (
+        <li>
+            <div className="nav_item_content">
+                <NavLink
+                    to={href}
+                    className={({ isActive }) => (isActive ? 'active' : '')}
+                >
+                    {label}
+                </NavLink>
+            </div>
+            {children && <div className="dropdown">dropdown content</div>}
+            {children && (
+                <div className="dropdown">
+                    <Container>
+                        <DropdownContent submenuscontent={children} />
+                    </Container>
+                </div>
+            )}
+        </li>
+    );
+};
+
+export default MenuItem;
